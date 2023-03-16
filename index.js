9593577a162b38bd972e62f07c06b5ce6146270a
Tofayel <mdtofayelhossain69@gmail.com>
2023-03-16 12:00:38 +0600

    simple server with a frist get api

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config()
+
+
+const app = express()
+const port = process.env.port || 5000 ;
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@photo-village.kapv1dn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run(){
+    const serviceCollection = client.db("photo_village").collection("photographer_services");
+
+
+    try{
+        // get APIs
+
+
+       app.get('/services',async(req,res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query)
+        const result= await cursor.toArray()
+        res.send(result)
+       })
+
+
+       app.get('/limitService',async(req,res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query).limit(3)
+        const result= await cursor.toArray()
+        res.send(result)
+       })
+
+      app.get('/servDetails/:id',(req,res)=>{
+        console.log(req)
+      })
+
+
+
+
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(error => console.log(error))
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('all ok your server >>> running')
+})
+
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
